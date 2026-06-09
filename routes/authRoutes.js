b1e16c6c1c5d70fa6c2a7b5af4baf8b9f8a49440
the developer <eloidev10@gmail.com>
2026-06-09 08:03:25 -0700

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -12,3 +12,28 @@ router.post("/register",registerTeacher);
 router.post("/login",loginTeacher);
 
 export default router;
+import express from "express";
+
+import Teacher from "../models/Teacher.js";
+
+const router = express.Router();
+
+router.get("/", async(req,res)=>{
+
+const teachers = await Teacher.find();
+
+res.json(teachers);
+
+});
+
+router.get("/:id", async(req,res)=>{
+
+const teacher = await Teacher.findById(
+req.params.id
+);
+
+res.json(teacher);
+
+});
+
+export default router;
